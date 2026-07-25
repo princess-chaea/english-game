7a717395dc7484ddbda97741e6588669e867010c
princess-chaea <dltjdrms320@gmail.com>
2026-07-26 00:36:28 +0900

    Remove temp script

--- find_tabs.py
+++
@@ -1,8 +0,0 @@
-﻿import sys
-
-with open('index.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-for i, l in enumerate(lines):
-    if 'id="gearTab"' in l or 'id="petTab"' in l or 'id="skillTab"' in l or 'id="hallOfFameTab"' in l or 'id="statsTab"' in l:
-        print(f'Line {i+2}: {lines[i+1].strip()}')

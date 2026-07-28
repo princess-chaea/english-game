12a301685c93056f1dab20d5390300244b3be653
princess-chaea <dltjdrms320@gmail.com>
2026-07-29 07:47:12 +0900

    Bump SW to v9 to force cache clear on all clients

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VOCA HERO! Service Worker - Edge Request & Cache Optimization
-const CACHE_NAME = 'vocahero-v8';
+const CACHE_NAME = 'vocahero-v9';
 const STATIC_ASSETS = [
   '/',
   '/index.html',

c92941dc4c26a4fc10c764ca2548bd659cee2a7d
princess-chaea <dltjdrms320@gmail.com>
2026-07-28 22:18:07 +0900

    Bump SW cache version to force PWA refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VOCA HERO! Service Worker - Edge Request & Cache Optimization
-const CACHE_NAME = 'vocahero-v4';
+const CACHE_NAME = 'vocahero-v5';
 const STATIC_ASSETS = [
   '/',
   '/index.html',

2f6ac53652ca6d1bc2179faebdcb4d9ce975155f
princess-chaea <dltjdrms320@gmail.com>
2026-07-28 23:27:34 +0900

    Bust PWA cache: bump sw.js to vocahero-v6 to force clients to reload fresh assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VOCA HERO! Service Worker - Edge Request & Cache Optimization
-const CACHE_NAME = 'vocahero-v5';
+const CACHE_NAME = 'vocahero-v6';
 const STATIC_ASSETS = [
   '/',
   '/index.html',

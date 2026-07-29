fb3d172c9548daa953ebcd0bf505d33897943108
princess-chaea <dltjdrms320@gmail.com>
2026-07-29 11:22:00 +0900

    Bump SW cache to v12

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿// VOCA HERO! Service Worker - Edge Request & Cache Optimization
-const CACHE_NAME = 'vocahero-v11';
+const CACHE_NAME = 'vocahero-v12';
 const STATIC_ASSETS = [
   '/',
   '/index.html',

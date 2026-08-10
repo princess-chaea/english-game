e13ecc1a7208ab29c0c0de14d882cbd585b64822
user <dltjdrms320@gmail.com>
2026-08-10 16:18:07 +0900

    fix: request 에러 응답 details 문자열 연결 보완 및 실시간 진단

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VOCA HERO! Service Worker - Edge Request & Cache Optimization
-const CACHE_NAME = 'vocahero-v67';
+const CACHE_NAME = 'vocahero-v68';
 const STATIC_ASSETS = [
   '/',
   '/index.html',

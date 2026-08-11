48b74b7d96e83289650d1601d13662a4025bffd5
user <dltjdrms320@gmail.com>
2026-08-12 06:22:21 +0900

    fix: reveal guild summon hero cards

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VOCA HERO! Service Worker - Edge Request & Cache Optimization
-const CACHE_NAME = 'vocahero-v91';
+const CACHE_NAME = 'vocahero-v92';
 const STATIC_ASSETS = [
   '/',
   '/index.html',

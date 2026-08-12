61ac3fd0cdb51cbab2286827deec68e69e2c6361
user <dltjdrms320@gmail.com>
2026-08-12 18:16:30 +0900

    fix: compact dimensional shard unlock buttons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VOCA HERO! Service Worker - Edge Request & Cache Optimization
-const CACHE_NAME = 'vocahero-v101';
+const CACHE_NAME = 'vocahero-v102';
 const STATIC_ASSETS = [
   '/',
   '/index.html',

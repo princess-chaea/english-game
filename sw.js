0bb2fab45c4ba94e5875504f347affefce7e0aa3
user <dltjdrms320@gmail.com>
2026-08-13 19:56:02 +0900

    fix: allow hall of fame mobile scrolling

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VOCA HERO! Service Worker - Edge Request & Cache Optimization
-const CACHE_NAME = 'vocahero-v112';
+const CACHE_NAME = 'vocahero-v113';
 const STATIC_ASSETS = [
   '/',
   '/index.html',

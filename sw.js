b56e04682883e1c678159bef472ee6a50325f1e6
user <dltjdrms320@gmail.com>
2026-08-24 10:20:54 +0900

    feat: refine teacher trials and theme controls

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VOCA HERO! Service Worker - Edge Request & Cache Optimization
-const CACHE_NAME = 'vocahero-v118';
+const CACHE_NAME = 'vocahero-v119';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
